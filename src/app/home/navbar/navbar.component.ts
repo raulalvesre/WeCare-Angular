02fe1bf8896afae91105dfbdd99b6658912d6183
Rony <ronymesquitadasilva@gmail.com>
2023-05-24 00:34:40 -0300

    Changes: • Updated candidate registration to include qualifications and causes • Changed menu to works with the logged-in user

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AccessService } from 'src/shared/services/access.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  loginSubscription: Subscription;
+
   isMenuCollapsed = true;
   // mock pra nav bar
   //opções:
@@ -13,7 +17,26 @@ export class NavbarComponent {
   //volunteer
   //institution
   //unknow
-  userMenuType: string = "institution"
+  userMenuType: string = 'unknow';
+
+  constructor(private accessService: AccessService) {
+    this.loginSubscription = accessService.loginNotification()
+      .subscribe(loggedIn => {
+        if (loggedIn) {
+          this.configureMenuForCurrentUser();
+        } else {
+          this.userMenuType = 'unknow';
+        }
+      });
+  }
+
+  ngOnInit(): void {
+    this.configureMenuForCurrentUser();
+  }
+
+  ngOnDestroy(): void {
+    this.loginSubscription.unsubscribe();
+  }
 
   toggleNavigationMenu() {
     this.isMenuCollapsed = !this.isMenuCollapsed;
@@ -21,5 +44,19 @@ export class NavbarComponent {
 
   closeNavigationMenu() {
     this.isMenuCollapsed = true;
+  }
+
+  private configureMenuForCurrentUser() {
+    const currentUser = this.accessService.getCurrentUser();
+
+    if (currentUser == null) {
+      this.userMenuType = 'unknow';
+    } else if ('cnpj' in currentUser) {
+      this.userMenuType = 'institution';
+    } else {
+      this.userMenuType = 'volunteer';
+    }
+
+    // TODO adicionar a checagem para o acesso adm
   }
 }
--- src/shared/services/access.service.ts
+++ src/shared/services/access.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { decodeJwt } from 'jose';
 import { environment } from 'src/environments/environment';
 import { UserDocumentType } from '../models/document-type.model';
@@ -16,18 +16,26 @@ import { Candidate } from '../models/candidate.model';
 export class AccessService {
   private readonly apiUrl = environment.apiUrl;
 
+  private loginSubject: BehaviorSubject<boolean>;
+
   private readonly tokenStorageKey = 'token';
   private readonly userIdKey = 'id';
   private readonly emailKey = 'email';
   private readonly userRoleKey = 'role';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+    this.loginSubject = new BehaviorSubject(localStorage.getItem(this.tokenStorageKey) != null);
+  }
+
+  loginNotification(): Observable<boolean> {
+    return this.loginSubject.asObservable();
+  }
 
   login(accessCredential: AccessCredential): Observable<JwtToken> {
     const authenticationUrl = accessCredential.documentType === UserDocumentType.cpf
       ? `${this.apiUrl}/api/auth/login-candidate`
       : `${this.apiUrl}/api/auth/login-institution`;
-      
+
     return this.httpClient
       .post<JwtToken>(authenticationUrl, {
         email: accessCredential.email,
@@ -41,7 +49,13 @@ export class AccessService {
         localStorage.setItem(this.userIdKey, (jwtPayload as any).id || (jwtPayload as any).Id);
         localStorage.setItem(this.emailKey, (jwtPayload as any).email);
         localStorage.setItem(this.userRoleKey, (jwtPayload as any).role);
+
+        this.loginSubject.next(true);
       }));
+  }
+
+  logout() {
+    localStorage.clear();
   }
 
   register(userRegistration: UserRegistration): Observable<HttpResponse<any>> {
@@ -75,8 +89,12 @@ export class AccessService {
     return localStorage.getItem(this.tokenStorageKey);
   }
 
-  getCurrentUser(): Institution | Candidate {
+  getCurrentUser(): Institution | Candidate | null {
     const userRole = localStorage.getItem(this.userRoleKey);
+
+    if (userRole == null) {
+      return null;
+    }
 
     if (userRole === 'CANDIDATE') {
       return {
