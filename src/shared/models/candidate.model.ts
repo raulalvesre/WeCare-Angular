5a3d6b53469f9b64aaa79ceac10c7342676afe0a
Rony <ronymesquitadasilva@gmail.com>
2023-06-06 03:06:08 -0300

    Edited profile page to allow update candidate profile

--- src/shared/models/candidate.model.ts
+++ src/shared/models/candidate.model.ts
@@ -1,4 +1,16 @@
+import { Address } from "./address.model";
+import { OpportunityCause } from "./opportunity-cause.model";
+import { Qualification } from "./qualification.model";
+
 export interface Candidate {
   id: number;
   email: string;
+  name: string;
+  telephone: string;
+  bio: string;
+  address: Address;
+  cpf: string;
+  birthDate: string;
+  qualificationsIds?: Qualification[] | number[];
+  interestedInCausesIds?: OpportunityCause[] | number[];
 }
