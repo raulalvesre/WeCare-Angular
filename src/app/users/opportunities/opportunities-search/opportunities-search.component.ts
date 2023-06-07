e3d7da70b3c0447d2cb989b55be81b6f5328bd48
Rony <ronymesquitadasilva@gmail.com>
2023-06-06 23:52:02 -0300

    Added filter options to opportunity search in candidate opportunities page

--- src/app/users/opportunities/opportunities-search/opportunities-search.component.ts
+++ src/app/users/opportunities/opportunities-search/opportunities-search.component.ts
@@ -4,8 +4,15 @@ import { Page } from 'src/shared/models/page.model';
 import { VolunteerOpportunity } from 'src/shared/models/volunteer-opportunity.model';
 import { ToastService } from 'src/shared/services/toast.service';
 import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
-import { FileService } from 'src/shared/services/file.service';
-import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FederativeUnit } from 'src/shared/models/address.model';
+import { OpportunityCause } from 'src/shared/models/opportunity-cause.model';
+import { delay } from 'rxjs';
+import { ViaCepService } from 'src/shared/services/via-cep.service';
+import { OpportunityCauseService } from 'src/shared/services/opportunity-cause.service';
+import { AccessService } from 'src/shared/services/access.service';
+import { ImageService } from 'src/shared/services/image.service';
 
 
 @Component({
@@ -14,23 +21,124 @@ import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstra
   styleUrls: ['./opportunities-search.component.css']
 })
 export class OpportunitiesSearchComponent implements OnInit {
-
   readonly opportunityDescriptionLength = 50;
 
+  searchForm: FormGroup;
+
   volunteerOpportunities: VolunteerOpportunity[] = [];
+
+  opportunityCauses: OpportunityCause[] = [];
+  selectedOpportunityCauses: OpportunityCause[] = [];
+
+  federativeUnits: FederativeUnit[] = [];
+  selectedFederativeUnits: FederativeUnit[] = [];
+
+  initialDate?: Date;
+  finalDate?: Date;
 
   pageNumber = 1;
   pageSize = 10;
   hasNextPage = false;
 
+  searching = true;
+
   constructor(
     private volunteerOpportunityService: VolunteerOpportunityService,
     private toastService: ToastService,
-    private fileService: FileService,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private viaCepService: ViaCepService,
+    private opportunityCauseService: OpportunityCauseService,
+    private accessService: AccessService,
+    public imageService: ImageService,
   ) { }
 
   ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      initialDate: new FormControl(),
+      finalDate: new FormControl(),
+    });
+
+    this.loadOpportunities();
+
+    this.searchCauses();
+
+    this.searchFederativeUnits();
+
+    this.searchForm.get('initialDate')
+      .valueChanges
+      .pipe(delay(200))
+      .subscribe(initialDate => {
+        if (new Date(initialDate) instanceof Date) {
+          this.initialDate = new Date(initialDate);
+        }
+
+        setTimeout(() => {
+          this.loadOpportunities();
+        }, 1500);
+      });
+
+    this.searchForm.get('finalDate')
+      .valueChanges
+      .pipe(delay(200))
+      .subscribe(finalDate => {
+        if (new Date(finalDate) instanceof Date) {
+          this.finalDate = new Date(finalDate);
+        }
+
+        setTimeout(() => {
+          this.loadOpportunities();
+        }, 1500);
+      });
+  }
+
+  addCauseToSearch(opportunityCause: OpportunityCause) {
+    const opportunityIndex = this.selectedOpportunityCauses.indexOf(opportunityCause);
+    if (opportunityIndex >= 0) {
+      return;
+    }
+
+    this.selectedOpportunityCauses.push(opportunityCause);
+
+    this.loadOpportunities();
+  }
+
+  removeOpportunityCause(opportunityCause) {
+    const opportunityIndex = this.selectedOpportunityCauses.indexOf(opportunityCause);
+    this.selectedOpportunityCauses.splice(opportunityIndex, 1);
+
+    this.loadOpportunities();
+  }
+
+  addFederativeUnitToSearch(federativeUnit: FederativeUnit) {
+    // const federativeUnitIndex = this.selectedFederativeUnits.indexOf(federativeUnit);
+    // if (federativeUnitIndex >= 0) {
+    //   return;
+    // }
+
+    // this.selectedFederativeUnits.push(federativeUnit);
+
+
+    this.selectedFederativeUnits = [federativeUnit];
+
+    this.loadOpportunities();
+  }
+
+  removeFederativeUnit(federativeUnit: FederativeUnit) {
+    const federativeUnitIndex = this.selectedFederativeUnits.indexOf(federativeUnit);
+    this.selectedFederativeUnits.splice(federativeUnitIndex, 1);
+
+    this.loadOpportunities();
+  }
+
+  clearDatesSelection() {
+    this.searchForm.get('initialDate')
+      .setValue(null, { onlySelf: true, emitEvent: false });
+    this.searchForm.get('finalDate')
+      .setValue(null, { onlySelf: true, emitEvent: false });
+
+    this.initialDate = null;
+    this.finalDate = null;
+
     this.loadOpportunities();
   }
 
@@ -57,6 +165,8 @@ export class OpportunitiesSearchComponent implements OnInit {
           if (httpResponse.status == HttpStatusCode.NoContent) {
             this.toastService.show('Interesse cadastrado com sucesso', { classname: 'bg-success text-light', delay: 5000 });
             this.toastService.show('Pendente de aprovação', { classname: 'bg-info text-light', delay: 5000 });
+
+            this.loadOpportunities();
           }
         },
         error: (httpErrorResponse: HttpErrorResponse) => {
@@ -80,19 +190,26 @@ export class OpportunitiesSearchComponent implements OnInit {
       });
   }
 
-  convertBase64ToPhotoUrl(photoBase64: string) {
-    const photoExtension = this.fileService.fileExtension(photoBase64);
-    return `data:image/${photoExtension};base64,${photoBase64}`;
+  openVolunteerOpportunityModal(volunteerOpportunityModal) {
+    this.modalService.open(volunteerOpportunityModal, { size: 'xl', centered: true });
   }
 
-  openXl(content) {
-		this.modalService.open(content, { size: 'xl' , centered: true});
-	}
-
   private loadOpportunities() {
+    const currentUser = this.accessService.getCurrentUser();
+
+    this.searching = true;
+
+    this.pageNumber = 1;
+    this.volunteerOpportunities = [];
+
     this.volunteerOpportunityService.search({
       pageNumber: this.pageNumber,
-      pageSize: this.pageSize
+      pageSize: this.pageSize,
+      candidateNotRegistered: currentUser.id,
+      opportunityCauses: this.selectedOpportunityCauses,
+      federativeUnits: this.selectedFederativeUnits,
+      initialDate: this.initialDate,
+      finalDate: this.finalDate
     })
       .subscribe({
         next: (page: Page<VolunteerOpportunity[]>) => {
@@ -111,6 +228,28 @@ export class OpportunitiesSearchComponent implements OnInit {
             }
           }
         }
+      })
+      .add(() => {
+        this.searching = false;
+      });
+  }
+
+  private searchCauses() {
+    this.opportunityCauseService.search()
+      .subscribe({
+        next: opportunityCauses => {
+          this.opportunityCauses = opportunityCauses;
+        },
+        error: error => {
+          console.error(error);
+        }
+      });
+  }
+
+  private searchFederativeUnits() {
+    this.viaCepService.getFederativeUnits()
+      .subscribe(federativeUnits => {
+        this.federativeUnits = federativeUnits;
       });
   }
 
